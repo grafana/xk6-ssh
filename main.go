b9e87e2cd7942a9424baa2e52bc36a0a22192737
Alexey Kuznetsov <m2lxkuz@gmail.com>
2021-03-21 02:34:24 +0500

    Init the project

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
-package ssh
+package xk6ssh
 
-import (
-	"github.com/loadimpact/k6/js/modules"
-)
+import "github.com/loadimpact/k6/js/modules"
 
 const version = "v0.0.1"
 
 func init() {
-	modules.Register("k6/x/ssh", &SSH{
-		Version: version,
-	})
-}
-
-// SSH is the main export of k6 docker extension
-type SSH struct {
-	Version string
+	modules.Register("k6/x/ssh", &K6SSH{Version: version})
 }
