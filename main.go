b3abf0ef3cedbd333809c155fdd37a6bfefff868
Paul Balogh <javaducky@gmail.com>
2023-04-04 12:59:33 -0500

    Add CI workflows and update to correct linter issues
    Fixes #9
    
    Signed-off-by: Paul Balogh <javaducky@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
+// Package xk6ssh registers the xk6-ssh javascript extension
 package xk6ssh
 
-import "go.k6.io/k6/js/modules"
+import (
+	"github.com/spf13/afero"
+	"go.k6.io/k6/js/modules"
+)
 
 func init() {
-	modules.Register("k6/x/ssh", &K6SSH{})
+	modules.Register("k6/x/ssh", &K6SSH{fs: afero.NewOsFs()})
 }
